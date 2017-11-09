2816d67162bfecf589130a24bf2cd8d37680df2d
Aimad Harit <aimad.harit@ekino.com>
2017-11-09 17:49:20 +0100

    implement http/1.1 to compare w/ http/2

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+/**
+ * Proof of concept
+ * Http2 using speedy module support http1.1, http2, spdy
+ *
+ */
 const spdy = require('spdy')
 const express = require('express')
 const fs = require('mz/fs')
+const yargs = require('yargs')
 
 const app = express()
 
+const args = yargs.usage('Usage: $0 <command> [options]')
+    .describe('protocol', 'Choose which protocol to use [http/1.1, http/2]')
+    .alias('p', 'protocol')
+    .help()
+    .argv
+
+const HTTP2 = 'http/2'
+const PORT = 8000
 //define assets for push feature
 const assets = [
     { path: 'public/assets/js/misc.js',    res: {'content-type': 'application/javascript'}},
@@ -16,38 +30,46 @@ app.use(express.static('public'))
  *   Send assets before sending anything else
  *   speed-up load page
  */
-app.get('/', (req, res) => {
-    Promise.all(assets.map(asset => fs.readFile(asset)))
-        .then(files => {
-            //ensure Push feature is up
-              if (req.push){
-                  //send assets
-                  files.forEach(file => {
-                      //setup stream push
-                      const stream = res.push(file.path, { req: {'accept': '**/*'}, res: file.res})
+if (args.protocol === HTTP2) {
+    app.get('/', (req, res) => {
+        Promise.all(assets.map(asset => fs.readFile(asset)))
+            .then(files => {
+                //ensure Push feature is up
+                if (req.push) {
+                    //send assets
+                    files.forEach(file => {
+                        //setup stream push
+                        const stream = res.push(file.path, {req: {'accept': '**/*'}, res: file.res})
 
-                      //catch err
-                      stream.on('error', err => {
-                          console.log(err);
-                      })
+                        //catch err
+                        stream.on('error', err => {
+                            console.log(err);
+                        })
 
-                      //ending stream here
-                      stream.end(file);
-                  })
+                        //ending stream here
+                        stream.end(file);
+                    })
 
-                  //send then index when all deps are resolved
-                  fs.readFile('public/index.html')
-                      .then(indexFile => {
-                          res.end(indexFile)
-                      }).catch(err => res.status(500).send(err.toString()))
-              }
+                    //send then index when all deps are resolved
+                    fs.readFile('public/index.html')
+                        .then(indexFile => {
+                            res.end(indexFile)
+                        }).catch(err => res.status(500).send(err.toString()))
+                }
 
-        })
-})
+            })
+            .catch(err => res.status(500).send(err.toString()))
+    })
+} else {
+    app.get('/', (req, res) => {
+        fs.readFile('public/index.html')
+            .then(indexFile => {
+                res.status(200).send(indexFile)
+            })
+    })
+}
 
 /**
- *  Testing multiplexing by setting 2 routes
- *
  *  GET  /me return user information
  *  GET  /products return user product
  *
@@ -73,13 +95,20 @@ app.get('/product', (req, res) => {
     res.status(200).send(data)
 })
 
+if (args.protocol === HTTP2) {
+    spdy.createServer({
+        key: fs.readFileSync('./server.key'),
+        cert: fs.readFileSync('./server.crt')
+    }, app)
+        .listen(PORT, err => {
+            if (err) throw err
+            console.log(`Server http/2 started on port 8080`)
+        })
 
-spdy.createServer({
-    key:  fs.readFileSync('./server.key'),
-    cert: fs.readFileSync('./server.crt')
-}, app)
-    .listen(8000, err => {
+} else {
+    //start standard express server
+    app.listen(PORT, err => {
         if (err) throw err
-        console.log('Server start on port 8080')
+        console.log(`Server http/1.1 started on port 8080`)
     })
-    
+}
